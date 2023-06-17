3b876fa1c8da4a714d9ae9c9157d7d6ae87a2a85
mesarikaya <mesarikaya@gmail.com>
2023-06-17 23:11:25 +0200

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,12 @@
 terraform {
 
-  cloud {
-    organization = "organization-name"
+  /* cloud {
+    organization = "solid-software-creations"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
-  }
+  } */
 
   required_providers {
     aws = {
